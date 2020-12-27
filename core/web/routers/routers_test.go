22ae62a57b03984e1a4fac051ba9c08118ad32de
evaccino <634134753@qq.com>
2020-12-27 17:47:23 +0800

    初始化K8S模块

--- core/web/routers/routers_test.go
+++ core/web/routers/routers_test.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"Kinux/core/k8s"
 	"Kinux/tools/cfg"
 	"github.com/spf13/viper"
 )
@@ -9,4 +10,6 @@ func init() {
 	cfg.InitConfig(func(v *viper.Viper) {
 		v.AddConfigPath("../../../")
 	})
+	cfg.DefaultConfig.Kubernetes.KubeConfigPath = "../../../kubeConfig"
+	k8s.InitKubernetes()
 }
