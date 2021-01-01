d005e90ad48055e1992be7e9d517f0063301e246
evaccino <634134753@qq.com>
2021-01-01 15:38:48 +0800

    修改函数名

--- core/web/services/mission_controller.go
+++ core/web/services/mission_controller.go
@@ -89,19 +89,19 @@ func (mc *MissionController) NewDeployment() (err error) {
 
 // 清除用户正在进行的Deployment
 func (mc *MissionController) ClearAllMission() (err error) {
-	return mc.destroyMission(nil)
+	return mc.destroyDeployment(nil)
 }
 
 // 删除用户指定的dp
-func (mc *MissionController) DestroyMission() (err error) {
+func (mc *MissionController) DestroyDeployment() (err error) {
 	if mc.Mission == nil || mc.Mission.ID == 0 {
 		return errors.New("无法删除用户指定的任务: 任务信息未初始化")
 	}
-	return mc.destroyMission(NewLabelMarker().WithMission(mc.Mission.ID))
+	return mc.destroyDeployment(NewLabelMarker().WithMission(mc.Mission.ID))
 }
 
 // 删除操作的内部实现
-func (mc *MissionController) destroyMission(l *labelMaker) (err error) {
+func (mc *MissionController) destroyDeployment(l *labelMaker) (err error) {
 	mc.appendOpt(func(mc *MissionController) error {
 		if mc.Ac == nil {
 			return errors.New("无法删除用户指定的任务: 用户信息未初始化")
