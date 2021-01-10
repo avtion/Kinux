19dd135ee41f30967e513181876e29ceaccea978
Avtion <mail@avtion.cn>
2021-01-10 21:37:02 +0800

    修复登陆接口因为鉴权中间件导致无法访问

--- core/web/routers/v1.go
+++ core/web/routers/v1.go
@@ -12,15 +12,24 @@ func init() {
 
 func v1Routers() initFunc {
 	return func(r *gin.Engine) {
-		v1 := r.Group("v1", middlewares.JsonWebTokenAuth)
+		v1 := r.Group("v1")
 
-		// WebSocket
-		v1.GET("/ws", controllers.WebSocketHandlerV1)
+		// 用户登陆接口不使用鉴权中间件
+		v1.POST("/account/login", controllers.LoginAccount)
+
+		// 挂载JWT鉴权中间件
+		v1 = v1.Group("", middlewares.JsonWebTokenAuth)
 
 		// Account
-		ac := v1.Group("/account")
+		//ac := v1.Group("/account")
 		{
-			ac.POST("/login", controllers.LoginAccount)
+			//ac.POST("/login", controllers.LoginAccount)
+		}
+
+		// WebSocket
+		ws := v1.Group("/ws")
+		{
+			ws.GET("/", controllers.WebSocketHandlerV1)
 		}
 
 		// 任务相关
