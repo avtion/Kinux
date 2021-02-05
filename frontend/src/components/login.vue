484a1ff71d1b789a44446875353653d8e608d109
Avtion <mail@avtion.cn>
2021-02-05 19:21:06 +0800

    更新登陆页面

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -100,21 +100,24 @@
 </template>
 
 <script lang="ts" type="module">
-import { reactive, ref, defineComponent, toRaw } from "vue"
+import { reactive, ref, defineComponent, onBeforeMount } from "vue";
 
 // antd
-import { useForm } from "@ant-design-vue/use"
-import { UserOutlined, LockOutlined } from "@ant-design/icons-vue"
-import { notification } from "ant-design-vue"
+import { useForm } from "@ant-design-vue/use";
+import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
+import { notification } from "ant-design-vue";
 
 // api
-import { Account } from "../apis/user"
+import { Account } from "@api/user";
 
 // 背景图
-import backgroundImg from "../assets/images/login-wallpaper.png"
+import backgroundImg from "@image/login-wallpaper.png";
 
 // store
-import { GetStore } from "../store/store"
+import { GetStore } from "@/store/store";
+
+// vue-router
+import { useRouter } from "vue-router";
 
 // 账号
 interface account {
@@ -126,49 +129,64 @@ export default defineComponent({
   name: "login",
   components: { UserOutlined, LockOutlined },
   setup(props, ctx) {
+    // vue相关变量
+    const store = GetStore();
+    const router = useRouter();
+
+    // 页面载入时先判断JWT是否有效
+    onBeforeMount(() => {
+      const token = store.getters.GetJWTToken;
+      if ((token as string) && token != "") {
+        console.log("存在JWT密钥，正在与服务端进行校验");
+        // TODO 密钥校验
+        notification.success({ message: "您已经成功登陆" });
+        router.push("dashboard");
+      }
+    });
+
     // 登陆状态进行时
-    const isLoging = ref(false)
+    const isLoging = ref(false);
 
     // 表单数据
     const loginFormData: account = reactive({
       username: "",
       password: "",
-    })
+    });
 
     // 表单校验规则
     const loginFormRules = reactive({
       username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
       password: [{ required: true, message: "请输入密码", trigger: "blur" }],
-    })
+    });
 
     // 登陆表单useForm解构
     const { resetFields, validate, validateInfos } = useForm(
       loginFormData,
       loginFormRules
-    )
-
-    const store = GetStore()
+    );
 
     // 登陆按钮触发函数
     const onSubmit = (e: { preventDefault: () => void }) => {
       e.preventDefault()
+
+      // 修改登陆状态
       validate()
         .then(() => {
-          // 修改登陆状态
-          isLoging.value = true
-
+          isLoging.value = true;
           // 登陆
           new Account(loginFormData.username, loginFormData.password)
             .login()
             .then(() => {
-              notification.success({ message: "登陆成功" })
-              // TOOD 路由跳转
-            })
+              notification.success({ message: "登陆成功" });
+              router.push("dashboard");
+            }).finally(() => {
+              isLoging.value = false;
+            });
         })
-        .finally(() => {
-          isLoging.value = false
-        })
-    }
+
+    };
+
+
 
     return {
       backgroundImg,
@@ -177,13 +195,13 @@ export default defineComponent({
       loginFormRules,
       onSubmit,
       validateInfos,
-    }
+    };
   },
-})
+});
 </script>
 
 <style>
-@import "../assets/css/tailwind@2.0.2.css";
+@import '../assets/css/tailwind@2.0.2.css';
 .form-item-custom {
   margin-bottom: 0px;
 }
