fab0fecf7e62f36e9e03b28cc642b8d1accf8105
Avtion <mail@avtion.cn>
2021-02-05 19:20:22 +0800

    修改Axios相关配置

--- frontend/src/apis/request.ts
+++ frontend/src/apis/request.ts
@@ -1,19 +1,23 @@
-import axios from "axios"
-import { AxiosRequestConfig } from "axios"
-import { notification } from "ant-design-vue"
+import axios, { AxiosResponse } from 'axios'
+import { AxiosRequestConfig } from 'axios'
+import { notification } from 'ant-design-vue'
+import { store } from '@/store/store'
+import routers from '@/routers/routers'
 
 // 默认Axios配置
-const config: AxiosRequestConfig = {
-  baseURL: "http://localhost:9001/",
+export const DefaultAxiosConfig: AxiosRequestConfig = {
+  baseURL: 'http://localhost:9001/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 15 * 1000,
+  timeoutErrorMessage: '无法与服务器建立链接',
 }
 
-// 路径
-export const paths: object = {
-  login: "v1/account/login",
-}
+interface headers {}
 
 // 默认客户端
-export const defaultClient = axios.create(config)
+export const defaultClient = axios.create(DefaultAxiosConfig)
 
 // 基础响应
 export class BaseResponse {
@@ -24,13 +28,13 @@ export class BaseResponse {
   constructor(obj: any) {
     if (
       obj == undefined ||
-      obj["code"] == undefined ||
-      obj["data"] == undefined
+      obj['code'] == undefined ||
+      obj['data'] == undefined
     ) {
       return
     }
-    this.Code = obj["code"]
-    this.Data = obj["data"]
+    this.Code = obj['code']
+    this.Data = obj['data']
   }
 
   // 后端规则约束
@@ -40,11 +44,26 @@ export class BaseResponse {
     }
     return true
   }
+
+  // 判断是否JWT鉴权失败
+  IsJWTAuthFailed(): boolean {
+    if (this.Code == respCode.JWTAuthFailed) {
+      return true
+    }
+    return false
+  }
+}
+
+// 响应代码
+enum respCode {
+  Success = 2000, // 通用成功
+  Failed = 4000, // 通用失败
+  JWTAuthFailed = 4001, // JWT鉴权失败
 }
 
 // 响应拦截器
 defaultClient.interceptors.response.use(
-  (response) => {
+  (response: AxiosResponse) => {
     // 判断结果是否存在
     if (response.data == undefined) {
       return
@@ -55,20 +74,65 @@ defaultClient.interceptors.response.use(
     if (!resp.IsSuccess()) {
       // 前端UI框架提示
       notification.error({
-        message: "请求失败",
+        message: '请求失败',
         description: resp.Data,
       })
 
+      if (resp.IsJWTAuthFailed) {
+        // JWT密钥失效则清空密钥缓存并跳转至登陆界面
+        console.log('JWT鉴权失效')
+        store.commit('ClearJWT')
+        routers.push('/')
+      }
+
       // 控制台输出
-      console.log("axios拦截服务端错误信息:", resp.Data)
+      console.log('axios拦截服务端错误信息:', resp.Data)
       return Promise.reject(resp)
     }
 
     // 成功则返回正确响应
     return response
   },
-  (error) => {
+  (err) => {
     // Do something with response error
-    return Promise.reject(error)
+    notification.error({
+      message: '服务器发生未知错误',
+    })
+    console.log(err)
+    return Promise.reject(err)
   }
 )
+
+// 请求拦截器 - 解决鉴权问题
+defaultClient.interceptors.request.use(
+  (cfg: AxiosRequestConfig): AxiosRequestConfig => {
+    const token = store.getters.GetJWTToken
+    if (<string>token != '') {
+      cfg.headers.Authorization = 'Bearer ' + <string>token
+    }
+    return cfg
+  }
+)
+
+// 路径
+export const paths: routePath = {
+  ac: {
+    login: 'v1/account/login',
+  },
+  ms: {
+    list: 'v1/mission/',
+  },
+}
+
+interface routePath {
+  ac: account
+  ms: mission
+}
+
+interface account {
+  login: string
+}
+
+interface mission {
+  list: string
+}
