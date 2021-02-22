4746cb3eec438be7c6884ddf0c125b95162ac552
Avtion <mail@avtion.cn>
2021-02-22 20:02:40 +0800

    新增角色翻译方法

--- core/web/models/role.go
+++ core/web/models/role.go
@@ -12,3 +12,17 @@ const (
 
 // 角色数组，用于遍历
 var RoleArray = [...]RoleIdentify{RoleAnonymous, RoleNormalAccount, RoleManager, RoleAdmin}
+
+// 角色翻译器
+func RoleTranslator(identify RoleIdentify) string {
+	switch identify {
+	case RoleNormalAccount:
+		return "学生"
+	case RoleManager:
+		return "教师"
+	case RoleAdmin:
+		return "系统管理员"
+	default:
+		return "游客"
+	}
+}
