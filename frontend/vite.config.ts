26fdc73313fe55e088b00e33c5bdd1fdea62a7fb
Avtion <mail@avtion.cn>
2021-02-23 13:20:07 +0800

    修改deprecated的设置

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import { resolve } from "path"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    "@": resolve(__dirname, "./src"),
-    "@api": resolve(__dirname, "./src/apis"),
-    "@image": resolve(__dirname, "./src/assets/images"),
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@api': resolve(__dirname, './src/apis'),
+      '@image': resolve(__dirname, './src/assets/images'),
+    },
   },
   plugins: [vue()],
 })
