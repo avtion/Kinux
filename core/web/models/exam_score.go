d1e466d55c6b2e4151838e99ac2c325466ecf89a
Avtion <mail@avtion.cn>
2021-02-26 23:55:31 +0800

    新增获取和删除方法

--- core/web/models/exam_score.go
+++ core/web/models/exam_score.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"context"
+	"errors"
 	"gorm.io/gorm"
 )
 
@@ -31,4 +32,78 @@ func NewExamScoreCallback(ac, exam, mission, checkpoint uint, container string) 
 			Container:  container,
 		}).Error
 	}
+}
+
+/*
+	实现 Score 接口
+*/
+var _ Score = (*ExamScore)(nil)
+
+func (m *ExamScore) ListScores(ctx context.Context, builder *PageBuilder,
+	filters ...ScoreFilter) (res []*ScoreListResult, err error) {
+	// 获取成绩的数据
+	db := GetGlobalDB().WithContext(ctx)
+	if builder != nil {
+		db = builder.build(db)
+	}
+	for _, filter := range filters {
+		db = filter(db)
+	}
+	var data = make([]*ExamScore, 0)
+	err = db.Find(&data).Error
+
+	var (
+		accountIDs    = make([]uint, 0, len(data))
+		missionIDs    = make([]uint, 0, len(data))
+		checkpointIDs = make([]uint, 0, len(data))
+		examIDs       = make([]uint, 0, len(data))
+	)
+	for _, v := range data {
+		accountIDs = append(accountIDs, v.Account)
+		missionIDs = append(missionIDs, v.Mission)
+		checkpointIDs = append(checkpointIDs, v.Checkpoint)
+		examIDs = append(examIDs, v.Exam)
+	}
+
+	acMapper, err := GetAccountsUsernameMapper(ctx, accountIDs...)
+	if err != nil {
+		return
+	}
+	mcMapper, err := GetMissionsNameMapper(ctx, missionIDs...)
+	if err != nil {
+		return
+	}
+	cpMapper, err := GetCheckpointsNameMapper(ctx, checkpointIDs...)
+	if err != nil {
+		return
+	}
+	exMapper, err := GetExamsNameMapper(ctx, examIDs...)
+	if err != nil {
+		return
+	}
+
+	res = make([]*ScoreListResult, 0, len(data))
+	for _, v := range data {
+		acName, _ := acMapper[v.Account]
+		mcName, _ := mcMapper[v.Mission]
+		cpName, _ := cpMapper[v.Checkpoint]
+		exName, _ := exMapper[v.Exam]
+		res = append(res, &ScoreListResult{
+			ID:             int(v.ID),
+			Account:        acName,
+			Exam:           exName,
+			Mission:        mcName,
+			CheckpointName: cpName,
+			Container:      v.Container,
+		})
+	}
+	return
+}
+
+func (m *ExamScore) DeleteScore(ctx context.Context) (err error) {
+	if m.ID == 0 {
+		return errors.New("id为空")
+	}
+	err = GetGlobalDB().WithContext(ctx).Unscoped().Delete(new(ExamScore), m.ID).Error
+	return
 }
--- core/web/models/mission_score.go
+++ core/web/models/mission_score.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"context"
+	"errors"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
@@ -47,5 +48,72 @@ func FindAllAccountFinishMissionScore(ctx context.Context, account, mission uint
 		db = db.Where("container IN ?", containers)
 	}
 	err = db.Pluck("checkpoint", &cpIDs).Error
+	return
+}
+
+/*
+	实现 Score 接口
+*/
+var _ Score = (*MissionScore)(nil)
+
+func (m *MissionScore) ListScores(ctx context.Context, builder *PageBuilder,
+	filters ...ScoreFilter) (res []*ScoreListResult, err error) {
+	// 获取成绩的数据
+	db := GetGlobalDB().WithContext(ctx)
+	if builder != nil {
+		db = builder.build(db)
+	}
+	for _, filter := range filters {
+		db = filter(db)
+	}
+	var data = make([]*MissionScore, 0)
+	err = db.Find(&data).Error
+
+	var (
+		accountIDs    = make([]uint, 0, len(data))
+		missionIDs    = make([]uint, 0, len(data))
+		checkpointIDs = make([]uint, 0, len(data))
+	)
+	for _, v := range data {
+		accountIDs = append(accountIDs, v.Account)
+		missionIDs = append(missionIDs, v.Mission)
+		checkpointIDs = append(checkpointIDs, v.Checkpoint)
+	}
+
+	acMapper, err := GetAccountsUsernameMapper(ctx, accountIDs...)
+	if err != nil {
+		return
+	}
+	mcMapper, err := GetMissionsNameMapper(ctx, missionIDs...)
+	if err != nil {
+		return
+	}
+	cpMapper, err := GetCheckpointsNameMapper(ctx, checkpointIDs...)
+	if err != nil {
+		return
+	}
+
+	res = make([]*ScoreListResult, 0, len(data))
+	for _, v := range data {
+		acName, _ := acMapper[v.Account]
+		mcName, _ := mcMapper[v.Mission]
+		cpName, _ := cpMapper[v.Checkpoint]
+		res = append(res, &ScoreListResult{
+			ID:             int(v.ID),
+			Account:        acName,
+			Exam:           "",
+			Mission:        mcName,
+			CheckpointName: cpName,
+			Container:      v.Container,
+		})
+	}
+	return
+}
+
+func (m *MissionScore) DeleteScore(ctx context.Context) (err error) {
+	if m.ID == 0 {
+		return errors.New("id为空")
+	}
+	err = GetGlobalDB().WithContext(ctx).Unscoped().Delete(new(MissionScore), m.ID).Error
 	return
 }
