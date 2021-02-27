83f9237006d5a8f73bf1a8d7d4e68aed2222447b
Avtion <mail@avtion.cn>
2021-02-27 17:44:10 +0800

    新增快速获取角色选项控制器

---
+++ core/web/controllers/role.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"Kinux/core/web/models"
+	"Kinux/core/web/msg"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// 快速获取角色选项
+func QuickListRoles(c *gin.Context) {
+	type resType struct {
+		ID   uint   `json:"id"`
+		Name string `json:"name"`
+	}
+	res := make([]*resType, 0, len(models.RoleArray))
+	for _, v := range models.RoleArray {
+		res = append(res, &resType{
+			ID:   v,
+			Name: models.RoleTranslator(v),
+		})
+	}
+	c.JSON(http.StatusOK, msg.BuildSuccess(res))
+}
