9f3ad55f372cd9d18ade2b2ef743281ffedbb009
Avtion <mail@avtion.cn>
2021-03-06 19:50:51 +0800

    新增若干考试实验相关控制器

--- core/web/controllers/exam.go
+++ core/web/controllers/exam.go
@@ -157,3 +157,129 @@ func GetExamsNamespaces(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, msg.BuildSuccess(res))
 }
+
+// list
+func ListExamMissions(c *gin.Context) {
+	params := &struct {
+		Page, Size int
+		Exam       uint
+		Mission    uint
+	}{
+		Page:    cast.ToInt(c.DefaultQuery("page", "1")),
+		Size:    cast.ToInt(c.DefaultQuery("size", "10")),
+		Exam:    cast.ToUint(c.DefaultQuery("exam", "")),
+		Mission: cast.ToUint(c.DefaultQuery("mission", "")),
+	}
+	data, err := models.ListExamMissions(c, params.Exam, params.Mission, models.NewPageBuilder(params.Page, params.Size))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, msg.BuildFailed(err))
+		return
+	}
+
+	// 转译一下
+	type resType struct {
+		ID       uint `json:"id"`
+		Exam     uint `json:"exam"`
+		Mission  uint `json:"mission"`
+		Percent  uint `json:"percent"`
+		Priority int  `json:"priority"`
+	}
+	var res = make([]*resType, 0, len(data))
+
+	for _, v := range data {
+		res = append(res, &resType{
+			ID:       v.ID,
+			Exam:     v.Exam,
+			Mission:  v.Mission,
+			Percent:  v.Percent,
+			Priority: v.Priority,
+		})
+	}
+	c.JSON(http.StatusOK, msg.BuildSuccess(res))
+}
+
+// count
+func CountExamMissions(c *gin.Context) {
+	params := &struct {
+		Exam    uint
+		Mission uint
+	}{
+		Exam:    cast.ToUint(c.DefaultQuery("exam", "")),
+		Mission: cast.ToUint(c.DefaultQuery("mission", "")),
+	}
+	res, err := models.CountExamMissions(c, params.Exam, params.Mission)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, msg.BuildFailed(err))
+		return
+	}
+	c.JSON(http.StatusOK, msg.BuildSuccess(res))
+}
+
+// delete
+func DeleteExamMission(c *gin.Context) {
+	if err := models.DeleteExamMission(c, cast.ToUint(c.Param("id"))); err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, msg.BuildFailed(err))
+		return
+	}
+	c.JSON(http.StatusOK, msg.BuildSuccess("考试实验删除成功"))
+}
+
+// add
+func AddExamMission(c *gin.Context) {
+	params := &struct {
+		Exam     uint `json:"exam" binding:"required"`
+		Mission  uint `json:"mission" binding:"required"`
+		Percent  uint `json:"percent" binding:"required"`
+		Priority int  `json:"priority" binding:"required"`
+	}{}
+	if err := c.ShouldBindJSON(params); err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, msg.BuildFailed(err))
+		return
+	}
+	if err := (&models.ExamMissions{
+		Exam:     params.Exam,
+		Mission:  params.Mission,
+		Percent:  params.Percent,
+		Priority: params.Priority,
+	}).Create(c); err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, msg.BuildFailed(err))
+		return
+	}
+	c.JSON(http.StatusOK, msg.BuildSuccess("创建成功"))
+}
+
+// edit
+func EditExamMission(c *gin.Context) {
+	params := &struct {
+		ID       uint `json:"id" binding:"required"`
+		Exam     uint `json:"exam" binding:"required"`
+		Mission  uint `json:"mission" binding:"required"`
+		Percent  uint `json:"percent" binding:"required"`
+		Priority int  `json:"priority" binding:"required"`
+	}{}
+	if err := c.ShouldBindJSON(params); err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, msg.BuildFailed(err))
+		return
+	}
+	if err := (&models.ExamMissions{
+		Model:    gorm.Model{ID: params.ID},
+		Exam:     params.Exam,
+		Mission:  params.Mission,
+		Percent:  params.Percent,
+		Priority: params.Priority,
+	}).Save(c); err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, msg.BuildFailed(err))
+		return
+	}
+	c.JSON(http.StatusOK, msg.BuildSuccess("修改成功"))
+}
+
+// 获取考试实验已经使用的比例
+func GetExamMissionUsedPercent(c *gin.Context) {
+	res, err := models.GetExamMissionsUsedPercent(c, cast.ToUint(c.Param("id")))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, msg.BuildFailed(err))
+		return
+	}
+	c.JSON(http.StatusOK, msg.BuildSuccess(res))
+}
