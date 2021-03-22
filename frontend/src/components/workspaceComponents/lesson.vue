a309bc9a8ff728c966ee084846fbe2fb599220d9
evaccino <634134753@qq.com>
2021-03-22 13:14:55 +0800

    完善课程选的组件

--- frontend/src/components/workspaceComponents/lesson.vue
+++ frontend/src/components/workspaceComponents/lesson.vue
@@ -1,22 +1,23 @@
 <template>
   <a-card title="课程选择" :bordered="false" :loading="isProjectDataLoading">
-    <a-list :data-source="data">
+    <a-list :data-source="departmentLessonData">
       <template #renderItem="{ item }">
         <a-list-item>
           <a
-            class="container mx-auto bg-gray-50 h-auto w-full p-4 space-y-1 rounded-lg hover:bg-gray-200"
+            @click="junmpToMission(item.id)"
+            class="container mx-auto bg-gray-50 h-auto w-full p-4 space-y-1 rounded-lg hover:bg-gray-200 shadow"
           >
-            <div class="block text-2xl font-extrabold font-sans relative">
-              <div class="inline-block text-blue-400">|</div>
-              <div class="inline-block ml-2 text-gray-700">
-                {{ item.title }}
+            <div class="block font-extrabold font-sans relative">
+              <div class="inline-block text-blue-400 text-2xl">|</div>
+              <div class="inline-block ml-2 text-gray-700 text-lg">
+                {{ item.name }}
               </div>
               <div class="inline-block absolute inset-y-0 right-0">></div>
             </div>
             <div
               class="block text-sm pr-8 text-opacity-5 font-mono leading-relaxed h-auto line-clamp-3"
             >
-              {{ item.content }}
+              {{ item.desc }}
             </div>
           </a>
         </a-list-item>
@@ -29,57 +30,78 @@
 // antd
 import { RightOutlined } from '@ant-design/icons-vue'
 
-interface DataItem {
-  title: string
-  content: string
-}
+// vue-request
+import { useRequest } from 'vue-request'
+import { BaseResponse, defaultClient } from '@/apis/request'
 
-const bodyStyle = {
-  'line-height': '22px',
-  height: '44px',
-  overflow: 'hidden',
-  '-webkit-line-clamp': '1',
-  ' text-overflow': 'ellipsis',
-  display: '-webkit-box',
-  '-webkit-box-orient': 'vertical',
+// store
+import { GetStore } from '@/store/store'
+
+// vue-router
+import { useRouter } from 'vue-router'
+
+// vue-router
+import { Profile } from '@/store/interfaces'
+
+const apiPath = {
+  list: '/v2/dl/list',
 }
-const data: DataItem[] = [
-  {
-    title: '计算机网络',
-    content:
-      '计算机网络是指将地理位置不同的具有独立功能的多台计算机及其外部设备，通过通信线路连接起来，在网络操作系统，网络管理软件及网络通信协议的管理和协调下，实现资源共享和信息传递的计算机系统。',
-  },
-  {
-    title: 'Title 2',
-    content: '',
-  },
-  {
-    title: 'Title 3',
-    content: '',
-  },
-  {
-    title: 'Title 4',
-    content: '',
-  },
-  {
-    title: 'Title 5',
-    content: '',
-  },
-  {
-    title: 'Title 6',
-    content: '',
-  },
-]
 
 export default {
   components: {
     RightOutlined,
   },
   setup(props, ctx) {
+    // vue相关变量
+    const store = GetStore()
+    const router = useRouter()
+
+    // 用户资料
+    const profile = <Profile>store.getters.GetProfile
+
+    // 获取课程数据
+    type ListParams = {
+      page: number
+      size: number
+      department: number
+    }
+    type ListResult = {
+      id: number
+      name: string
+      desc: string
+    }
+    const departmentLessonDataAPI = (params: ListParams) => {
+      return defaultClient.get<BaseResponse>(apiPath.list, {
+        params: params,
+      })
+    }
+    const getListParams = (): ListParams => {
+      return <ListParams>{
+        page: 0,
+        size: 0,
+        department: profile.dpID,
+      }
+    }
+    const {
+      data: departmentLessonData,
+      run: getListData,
+      loading: isListDataLoading,
+    } = useRequest(departmentLessonDataAPI, {
+      defaultParams: [getListParams()],
+      formatResult: (res): ListResults => {
+        return res.data.Data
+      },
+    })
+
+    // 实验跳转
+    const junmpToMission = (lesson: number) => {
+      router.push({ name: 'missionSelector', params: { lesson: lesson } })
+    }
+
     return {
       isProjectDataLoading: false,
-      data,
-      bodyStyle,
+      departmentLessonData,
+      junmpToMission,
     }
   },
 }
