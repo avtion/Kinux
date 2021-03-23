66ef38d0195c6a3cab9cbdff0ad0b5365b74612a
Avtion <manaitao@52tt.com>
2021-03-23 15:47:28 +0800

    移除创建检查点监听器方法

--- core/web/services/listener.go
+++ core/web/services/listener.go
@@ -1,10 +1,8 @@
 package services
 
 import (
-	"Kinux/core/web/models"
 	"context"
 	terminalReader "github.com/Nerdmaster/terminal"
-	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/sirupsen/logrus"
 	"io"
 )
@@ -72,98 +70,4 @@ func (l *TerminalListener) DebugPrint() {
 			logrus.WithField("line", line).Trace("监听器Debug输出")
 		}
 	}()
-}
-
-// 创建检查点监听器
-func NewWrapperForCheckpointCallback(ac *models.Account, exam *models.Exam,
-	mission *models.Mission, container string, checkpoints ...*models.Checkpoint) (opt WsPtyWrapperOption) {
-	if mission == nil || len(checkpoints) == 0 {
-		return
-	}
-
-	var (
-		inOpt, outOpt       WsPtyWrapperOption
-		inReader, outReader *TerminalListener
-		inMap, outMap       map[string]func(ctx context.Context) (err error)
-	)
-
-	for _, cp := range checkpoints {
-		switch cp.Method {
-		case models.MethodExec:
-			// 懒加载
-			if inOpt == nil || inReader == nil {
-				inOpt, inReader = NewPtyWrapperListenerOpt(ListenStdin)
-			}
-			if inMap == nil {
-				inMap = make(map[string]func(ctx context.Context) (err error))
-			}
-			if exam == nil {
-				inMap[cp.In] = models.NewMissionScoreCallback(ac.ID, mission.ID, cp.ID, container)
-			} else {
-				inMap[cp.In] = models.NewExamScoreCallback(ac.ID, exam.ID, mission.ID, cp.ID, container)
-			}
-		case models.MethodStdout:
-			// 懒加载
-			if outOpt == nil || outReader == nil {
-				outOpt, outReader = NewPtyWrapperListenerOpt(ListenStdout)
-			}
-			if outMap == nil {
-				outMap = make(map[string]func(ctx context.Context) (err error))
-			}
-			if exam == nil {
-				outMap[cp.Out] = models.NewMissionScoreCallback(ac.ID, mission.ID, cp.ID, container)
-			} else {
-				outMap[cp.Out] = models.NewExamScoreCallback(ac.ID, exam.ID, mission.ID, cp.ID, container)
-			}
-		case models.MethodTargetPort:
-			// TODO 完成Web端口监听
-			continue
-		default:
-			continue
-		}
-	}
-
-	// 监听方法
-	listenFn := func(listener *TerminalListener,
-		callbackMap map[string]func(ctx context.Context) (err error)) {
-		// 比较器
-		dmp := diffmatchpatch.New()
-		for {
-			select {
-			case <-listener.Context.Done():
-				return
-			default:
-			}
-			line, _err := listener.Reader.ReadLine()
-			if _err != nil {
-				logrus.WithField("err", _err).Error("监听器发生错误")
-				return
-			}
-			if callback, isExist := callbackMap[line]; isExist {
-				if _err := callback(listener.Context); _err != nil {
-					logrus.WithField("err", _err).Error("监听器执行回调函数失败")
-					continue
-				}
-				delete(callbackMap, line)
-			} else {
-				for k := range callbackMap {
-					diffs := dmp.DiffMain(line, k, false)
-					logrus.WithField("diff", dmp.DiffPrettyText(diffs)).Trace("指令差异")
-				}
-			}
-		}
-	}
-
-	return CombineWsPtyWrapperOptions(inOpt, outOpt, func(w *WsPtyWrapper) {
-		// 输入和输出监听
-		// TODO Web监听
-		for _, tmp := range []struct {
-			l *TerminalListener
-			m map[string]func(ctx context.Context) (err error)
-		}{{l: inReader, m: inMap}, {l: outReader, m: outMap}} {
-			if tmp.l != nil {
-				go listenFn(tmp.l, tmp.m)
-			}
-		}
-	})
 }
