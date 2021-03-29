d94abaed081005ebe8f7494f09e4afadda8c5efe
Avtion <manaitao@52tt.com>
2021-03-29 09:39:40 +0800

    新增向目标Websocket链接发送信息和侵入目标Websocket链接的终端方法

--- core/web/services/ws_schedule_center.go
+++ core/web/services/ws_schedule_center.go
@@ -1,13 +1,22 @@
 package services
 
 import (
+	"Kinux/core/web/msg"
 	"context"
 	"errors"
+	jsoniter "github.com/json-iterator/go"
 	"sync"
 )
 
+func init() {
+	RegisterWebsocketOperation(wsOpAttachOtherWsWriter, AttachTargetWs)
+}
+
 /* Websocket调度器中心 */
 var scheduleCenter = new(sync.Map)
+
+// 错误
+var TargetWebsocketNotExistErr = errors.New("目标Websocket链接不存在")
 
 // 注册websocket链接
 func RegisterWsConn(id int, ws *WebsocketSchedule) (err error) {
@@ -29,6 +38,7 @@ func RegisterWsConn(id int, ws *WebsocketSchedule) (err error) {
 	return
 }
 
+// Websocket链接信息
 type ScheduleCenterListResult struct {
 	ID          int    `json:"id"` // 用户ID
 	Username    string `json:"username"`
@@ -37,6 +47,7 @@ type ScheduleCenterListResult struct {
 	PtyMetaData string `json:"pty_meta_data"`
 }
 
+// 获取Websocket链接信息
 func ListScheduleCenterInfo(_ context.Context) (res []*ScheduleCenterListResult) {
 	res = make([]*ScheduleCenterListResult, 0)
 	scheduleCenter.Range(func(key, value interface{}) bool {
@@ -54,6 +65,42 @@ func ListScheduleCenterInfo(_ context.Context) (res []*ScheduleCenterListResult)
 			}(),
 		})
 		return true
+	})
+	return
+}
+
+// 向目标Websocket链接发送信息
+func SendMessageToTargetWs(_ context.Context, id int, text string) (err error) {
+	_ws, isExist := scheduleCenter.Load(id)
+	if !isExist {
+		return TargetWebsocketNotExistErr
+	}
+	ws, _ := _ws.(*WebsocketSchedule)
+	return ws.SendMsg(msg.BuildSuccess(text))
+}
+
+// 侵入目标Websocket链接的终端
+func AttachTargetWs(ws *WebsocketSchedule, any jsoniter.Any) (err error) {
+	// 解析数据
+	params := &struct {
+		TargetID int `json:"target_id"`
+	}{}
+	any.Get("data").ToVal(params)
+
+	// 获取目标Websocket链接
+	_ws, isExist := scheduleCenter.Load(params.TargetID)
+	if !isExist {
+		return TargetWebsocketNotExistErr
+	}
+	targetWs := _ws.(*WebsocketSchedule)
+
+	if targetWs.pty == nil {
+		return errors.New("目标Websocket链接无正在使用的终端")
+	}
+
+	// 初始化Pty装饰器
+	targetWs.pty.stdoutListener = ws.InitPtyWrapper(func(w *WsPtyWrapper) {
+		w.metaData = targetWs.pty.metaData
 	})
 	return
 }
