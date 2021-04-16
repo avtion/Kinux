3d90af3c6247ab471575c68f6fea666780ed07ec
Avtion <mail@avtion.cn>
2021-04-17 01:12:49 +0800

    新增管理员获取实验成绩接口

--- frontend/src/apis/score.ts
+++ frontend/src/apis/score.ts
@@ -55,23 +55,48 @@ export class Score {
     })
   }
 
+  // 管理员获取考试成绩
   GetExamScoreForAdmin = () => {
-    return new Promise((resolve: (res: ExamScoreForAdmin[]) => void, reject) => {
-      defaultClient
-        .get('/v2/score/exam/', {
-          params: {
-            dp: this.department,
-            lesson: this.lesson,
-            exam: this.exam,
-          },
-        })
-        .then((res: AxiosResponse<BaseResponse>) => {
-          resolve(<ExamScoreForAdmin[]>res.data.Data)
-        })
-        .catch((err) => {
-          reject(err)
-        })
-    })
+    return new Promise(
+      (resolve: (res: ExamScoreForAdmin[]) => void, reject) => {
+        defaultClient
+          .get('/v2/score/exam/', {
+            params: {
+              dp: this.department,
+              lesson: this.lesson,
+              exam: this.exam,
+            },
+          })
+          .then((res: AxiosResponse<BaseResponse>) => {
+            resolve(<ExamScoreForAdmin[]>res.data.Data)
+          })
+          .catch((err) => {
+            reject(err)
+          })
+      }
+    )
+  }
+
+  // 管理员获取实验成绩
+  GetMissionScoreForAdmin = () => {
+    return new Promise(
+      (resolve: (res: MissionScoreForAdmin[]) => void, reject) => {
+        defaultClient
+          .get('/v2/score/mission/', {
+            params: {
+              dp: this.department,
+              lesson: this.lesson,
+              mission: this.mission,
+            },
+          })
+          .then((res: AxiosResponse<BaseResponse>) => {
+            resolve(<MissionScoreForAdmin[]>res.data.Data)
+          })
+          .catch((err) => {
+            reject(err)
+          })
+      }
+    )
   }
 }
 
@@ -115,6 +140,25 @@ export interface ExamScoreForAdmin {
   exam_end_at: number
   score: number
   mission_scores: missionScore[]
+  total: number
+  pos: number
+  id: number
+  role: number
+  profile: number
+  username: string
+  real_name: string
+  department: string
+  department_id: number
+}
+
+export interface MissionScoreForAdmin {
+  mission_id: number
+  mission_name: string
+  mission_desc: string
+  finish_score_counter: number
+  all_score_counter: number
+  score: number
+  score_details: ScoreDetail[]
   total: number
   pos: number
   id: number
