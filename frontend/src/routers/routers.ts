97a4478029bfd7d027758ea09bb7aeb238895278
Avtion <mail@avtion.cn>
2021-04-30 23:26:46 +0800

    使用动态路由

--- frontend/src/routers/routers.ts
+++ frontend/src/routers/routers.ts
@@ -1,37 +1,37 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 // 路由
-import loginComponents from '@/components/login.vue' // 用户登陆界面
-import dashboardComponents from '@/components/dashboard.vue' // 操作界面
-import workspaceComponents from '@/components/workSpace.vue' // 工作间统计
-import shellComponents from '@/components/shell.vue' // 终端
-import departmentManagerComponents from '@/components/departmentManager.vue' // 班级管理
-import AccountManagerComponents from '@/components/accountManager.vue' // 用户管理
-import deploymentManagerComponents from '@/components/deploymentManager.vue' // 配置管理
-import checkpointManagerComponents from '@/components/checkpointManager.vue' // 检查点管理
-import missionManagerComponents from '@/components/missionManager.vue' // 实验管理
-import examManagerComponents from '@/components/examManager.vue' // 考试管理
+const loginComponents = () => import('@/components/login.vue') // 用户登陆界面
+const dashboardComponents = () => import('@/components/dashboard.vue') // 操作界面
+const workspaceComponents = () => import('@/components/workSpace.vue') // 工作间统计
+const shellComponents = () => import('@/components/shell.vue') // 终端
+const departmentManagerComponents = () => import('@/components/departmentManager.vue')// 班级管理
+const AccountManagerComponents = () => import('@/components/accountManager.vue') // 用户管理
+const deploymentManagerComponents = () => import('@/components/deploymentManager.vue')// 配置管理
+const checkpointManagerComponents = () => import('@/components/checkpointManager.vue')// 检查点管理
+const missionManagerComponents = () => import('@/components/missionManager.vue') // 实验管理
+const examManagerComponents = () => import('@/components/examManager.vue')// 考试管理
 
-import profileComponents from '@/components/profile.vue' // 个人资料
-import sessionManagerComponents from '@/components/sessionManager.vue' // 实验会话
-import lessonManagerComponents from '@/components/lessonManager.vue' // 课程管理
+const profileComponents = () => import('@/components/profile.vue') // 个人资料
+const sessionManagerComponents = () => import('@/components/sessionManager.vue') // 实验会话
+const lessonManagerComponents = () => import('@/components/lessonManager.vue')// 课程管理
 
 // workspcae
-import LessonSelector from '@/components/workspaceComponents/lesson.vue'
-import MissionSelector from '@/components/workspaceComponents/mission.vue'
-import ExamSelector from '@/components/workspaceComponents/exam.vue'
-import ExamMissionSelector from '@/components/workspaceComponents/examMission.vue'
+const LessonSelector = () => import('@/components/workspaceComponents/lesson.vue')
+const MissionSelector = () => import('@/components/workspaceComponents/mission.vue')
+const ExamSelector = () => import('@/components/workspaceComponents/exam.vue')
+const ExamMissionSelector = () => import('@/components/workspaceComponents/examMission.vue')
 
-import shellWatcherComponents from '@/components/shellWatcher.vue' // 终端监控
+const shellWatcherComponents = () => import('@/components/shellWatcher.vue')// 终端监控
 
 // 查询成绩
-import stuScore from '@/components/score/student.vue'
-import teaScore from '@/components/score/teacher.vue'
-import ex from '@/components/score/ex.vue'
-import ms from '@/components/score/ms.vue'
+const stuScore = () => import('@/components/score/student.vue')
+const teaScore = () => import('@/components/score/teacher.vue')
+const ex = () => import('@/components/score/ex.vue')
+const ms = () => import('@/components/score/ms.vue')
 
 // 统计
-import counter from '@/components/counter.vue'
+const counter = () => import('@/components/counter.vue')
 
 // 实验相关
 const workspaceChild = [
