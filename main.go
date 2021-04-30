a5981c4d22741032f42b9f09fb572899ec413df8
Avtion <mail@avtion.cn>
2021-04-30 20:31:32 +0800

    修复时区问题

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import (
 	"Kinux/core/web"
 	"Kinux/tools/cfg"
 	"Kinux/tools/health"
+	"github.com/sirupsen/logrus"
+	"time"
+	_ "time/tzdata"
 )
 
 func main() {
+	// 修复时区问题
+	var err error
+	time.Local, err = time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		logrus.Panic(err)
+	}
 	cfg.InitConfig()
 	health.InitHealCheck() // 活性探针
 	k8s.InitKubernetes()
